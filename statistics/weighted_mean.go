62ee5f1de2e7a8a236d4eb27fd53d8224e7cf1c2
Anton Antonov <ajantonov@gmail.com>
2020-09-09 22:06:44 +0300

    [TRIMMED MEAN] Implemented TestWeightedMeanShouldReturnErrorWhenInputArrayIsEmpty.

--- statistics/weighted_mean.go
+++ statistics/weighted_mean.go
@@ -1,5 +1,10 @@
 package statistics
 
+import "errors"
+
 func WeightedMean(inputs []float64, weights []float64) (float64, error) {
+	if 0 == len(inputs) {
+		return 0, errors.New("empty inputs parameter")
+	}
 	return 0, nil
 }
--- statistics/weighted_mean_test.go
+++ statistics/weighted_mean_test.go
@@ -8,8 +8,12 @@ Trimmed Mean Unit Tests
 
 1. Weighted Mean should return error when input array is empty
 2. Weighted Mean should return error when array with weight values is empty
+3. Weighted Mean should return error when sum of all weights is zero
+4. Weighted Mean should return 1 when weights are { 1 } and input values { 1 }
+5. Weighted Mean should return 2,666666667  when wights are { 1, 2, 3, 4, 5, 6 } and input values { 6, 5, 4, 3, 2, 1 }
 
 */
+
 func TestWeightedMeanShouldReturnErrorWhenInputArrayIsEmpty(t *testing.T) {
 
 	var inputs []float64
